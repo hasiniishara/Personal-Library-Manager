8ce56833395d24a28b356bbdaf588f33f844db8b
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-28 18:56:59 +0200

    Implement addBook() function.

---
+++ PersonalLibrary.js
@@ -0,0 +1,22 @@
+//Declare a library array
+let library = [];
+
+
+//This function describes books add to the library array.
+function addBook(title, author){
+    //push objects to the array
+    library.push({Title:title, Author:author, Readstatus:false});
+    console.log(library);
+}
+
+//This function describes
+
+
+
+
+
+
+
+
+//Call addBook Function
+addBook("Mr Loard","James");
