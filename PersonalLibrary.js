0acc4265f9578a597748234e0e49d78423a19a24
HasiniIshara <hasiniisharao4@gmail.com>
2024-02-28 19:10:58 +0200

    Implement markAsread() function.

--- PersonalLibrary.js
+++ PersonalLibrary.js
@@ -5,12 +5,21 @@ let library = [];
 //This function describes books add to the library array.
 function addBook(title, author){
     //push objects to the array
-    library.push({Title:title, Author:author, Readstatus:false});
+    library.push({ Title:title, Author:author, Readstatus:false });
     console.log(library);
 }
 
-//This function describes
+//This function describes, The book is read.
+function markAsread(title) {
+    //Getting index of exact matching item
+    const index = library.findIndex(data => data.Title === title);
+    console.log(index);
+    //Set true value to the read status
+    library[index].Readstatus = true;
+    console.log(library);
+}
 
+//This function describes remove of the books
 
 
 
@@ -20,3 +29,5 @@ function addBook(title, author){
 
 //Call addBook Function
 addBook("Mr Loard","James");
+markAsread("Mr Loard");
+console.log(library);
